286890762538d8ce2067dd8d0a27f726e0621d3b
agarnaas <agarnaas@gmail.com>
2017-01-09 15:41:10 +0100

    log test

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 exports.RESTFulClient = require('./lib/client');
 exports.RESTFulResource = require('./lib/resource');
+console.log("************************ RESTFUL CLIENT INDEX ************************************");
